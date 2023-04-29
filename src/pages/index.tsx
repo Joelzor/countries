5c7aba1a2d4c35b77f2616bf0c010b31c1b15a6d
Joel Watkins <eck087@yahoo.co.uk>
2023-04-29 17:21:17 +0100

    search now working fine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineSearch } from "react-icons/ai";
 import { IFormattedCountry } from "@/interfaces/formattedCountry";
 import CountryList from "@/components/CountryList";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 interface HomeProps {
   countries: IFormattedCountry[];
@@ -9,18 +9,15 @@ interface HomeProps {
 
 export default function Home({ countries }: HomeProps) {
   const [countriesList, setCountriesList] = useState(countries);
-  const [filters, setFilters] = useState({ query: "", region: "all" });
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
-
     if (query === "") {
       setCountriesList(countries);
       return;
     }
-
     const filtered = countries.filter((country) => {
-      return country.name.toLowerCase().includes(query.toLowerCase());
+      return country.name.toLowerCase().includes(query);
     });
 
     setCountriesList(filtered);
@@ -38,15 +35,6 @@ export default function Home({ countries }: HomeProps) {
     }
   };
 
-  // const handleChange = (
-  //   e: ChangeEvent<HTMLSelectElement | HTMLInputElement>
-  // ) => {
-  //   const value = e.target.value;
-  //   const name = e.target.name;
-
-  //   setFilters({ ...filters, [name]: value });
-  // };
-
   return (
     <>
       <div className="px-5 md:px-12 py-8 md:flex justify-between relative">
@@ -54,15 +42,11 @@ export default function Home({ countries }: HomeProps) {
           type="text"
           className="shadow-md py-4 pl-24 w-full mb-8 md:w-2/5 rounded"
           placeholder="Search for a country..."
-          name="search"
           onChange={handleSearch}
-
-          // onChange={handleSearch}
         />
         <AiOutlineSearch className="absolute text-xl text-neutral-400 top-[52px] left-[60px] md:left-[80px]" />
 
         <select
-          name="region"
           className="shadow-md py-4 pl-4 pr-16 self-start rounded"
           onChange={handleSelect}
         >
@@ -74,12 +58,14 @@ export default function Home({ countries }: HomeProps) {
           <option value="Oceania">Oceania</option>
         </select>
       </div>
-      {countriesList.length > 1 && <CountryList countries={countriesList} />}
-      {/* {countriesList.length < 1 && (
-        <div className="h-[70vh] flex items-center justify-center">
-          <h2 className="text-2xl">Your search returned no results</h2>
+      <CountryList countries={countriesList} />
+      {countriesList.length < 1 && (
+        <div className="h-[60vh] flex justify-center">
+          <h2 className="text-2xl">
+            Your search tragically returned no results
+          </h2>
         </div>
-      )} */}
+      )}
     </>
   );
 }
